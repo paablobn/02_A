84cde11092721c0bf33251ddfdf7b0f70ba4c332
Pablo Bruixola Navarro <pablobn@iMac-02.local>
2022-09-20 11:47:47 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ciclo_de_las_actividades/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ciclo_de_las_actividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
